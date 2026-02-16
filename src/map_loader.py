6845c0a373567ffdb9975408b055801806e96fd9
Gianni Lieuw-A-Soe <glieuw-a@f1r1s17.codam.nl>
2026-02-16 13:10:16 +0100

    deleted generator and return a list of line from the file we read, so that we get the whole map when we call the func and also assign it to a var int the class object in game file

--- src/map_loader.py
+++ src/map_loader.py
@@ -1,6 +1,4 @@
-# generator to display the map
+#return a list of lines to display the map
 def load__map(filename):
     with open(filename, "r") as fd:
-        for line in fd:
-            yield line.strip()
-
+        return [line.strip() for line in fd]

2b6b3405bfe4afa7339ffb0eed14503e413329b5
Yiannis Dimitrakopoulos <ydimitra@f0r1s9.codam.nl>
2026-02-17 16:21:50 +0100

    managed to open the window succesfully and added closing functionality using esc key, still need to fix exiting by pressing the exit button from the window

--- src/events.py
+++ src/events.py
@@ -1,10 +1,16 @@
-def key_hook(keycode, game):
-    print("Key:", keycode)
+import os
 
-def close_hook(param):
-    exit(0)
+def key_hook(keycode, game):
+    # exit using esc
+    if keycode == 65307:
+        os._exit(0)
+
+# needs fix
+def close_hook(game):
+    game.mlx_lib.mlx_destroy_window(game.mlx, game.win)
+    os._exit(0)
 
 def register_events(game):
-    mlx_key_hook(game.win, key_hook, game)
-    mlx_hook(game.win, 17, 0, close_hook, None)
+    game.mlx_lib.mlx_hook(game.win, 17, 0, close_hook, game)
+    game.mlx_lib.mlx_key_hook(game.win, key_hook, game)
 
--- src/game.py
+++ src/game.py
@@ -1,7 +1,29 @@
 import sys
 sys.path.append("./mlx_CLXV-2.2")
 
-from mlx import *
+import mlx
+print(type(mlx.Mlx()))
+print(mlx.Mlx())
+
+
+# def close(param):
+#     sys.exit(0)
+#
+# def handle_key(keycode, param):
+#     if keycode == 65307:  # ESC
+#         sys.exit(0)
+
+#Creating instance to access methods of the Mlx class
+m = mlx.Mlx()
+print(dir(m))
+
+# # Hook the window close (event 17)
+# m.mlx_hook(win_ptr, 17, 0, close, None)
+# m.mlx_key_hook(win_ptr, handle_key, None)
+#
+# print("Starting loop...")
+# m.mlx_loop(mlx_ptr)
+
 from src.map_loader import load_map
 from src.renderer import draw_map
 from src.events import register_events
@@ -9,11 +31,11 @@ from src.events import register_events
 class Game:
     def __init__(self, map_file):
         self.map = load_map(map_file)
-
-        self.mlx = mlx_init()
-        self.win = mlx_new_window(self.mlx, 800, 600, "Maze")
+        self.mlx_lib = m
+        self.mlx = m.mlx_init()
+        self.win = m.mlx_new_window(self.mlx, 800, 600, "Maze")
 
     def run(self):
         draw_map(self)
         register_events(self)
-        mlx_loop(self.mlx)
+        self.mlx_lib.mlx_loop(self.mlx)

72d1cee05f94beac6908471e73ee25fd1167eecc
yiannis <107484245+DimYiannis@users.noreply.github.com>
2026-02-26 17:51:57 +0100

    finally managed to resolve the issue with displaying properly on the window, fixed the file we parse fromand properly display maze and changes of the maze style, need to display 42 and the path

--- run_display.py
+++ run_display.py
@@ -1,38 +1,10 @@
-# """Launcher — run from the project root: python3 run_display.py maze.txt"""
-# import sys
-# import os
-#
-# sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
-#
-# from display.window import main
-#
-# main()
+"""Launcher — run from the project root: python3 run_display.py maze.txt"""
+import sys
+import os
 
-import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
-import mlx as mlx_module
 
-m = mlx_module.Mlx()
-mlx_ptr = m.mlx_init()
-win_ptr = m.mlx_new_window(mlx_ptr, 400, 400, "test")
-# img_ptr = m.mlx_new_image(mlx_ptr, 400, 400)
-#
-# buf, bpp, size_line, fmt = m.mlx_get_data_addr(img_ptr)
-# print(f"bpp={bpp} size_line={size_line} fmt={fmt} buf type={type(buf)}")
-#
-# # Fill entire image bright red
-# row = bytes([0, 0, 255, 0] * 400)   # BGRX: B=0 G=0 R=255
-# for y in range(400):
-#     buf[y * size_line : y * size_line + 400 * 4] = row
-# Draw directly to window — no image buffer
+from display.window import main
 
-for y in range(100, 200):
-    for x in range(100, 200):
-        m.mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0x0000FF)
-m.mlx_do_sync(mlx_ptr)
+main()
 
-# m.mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 0, 0)
-
-def close(p): os._exit(0)
-m.mlx_hook(win_ptr, 17, 0, close, None)
-m.mlx_loop(mlx_ptr)
--- src/mlx_test.py
+++ src/mlx_test.py
@@ -16,15 +16,39 @@ def close_hook(game):
 m = mlx.Mlx()
 mlx_ptr = m.mlx_init()
 win_ptr = m.mlx_new_window(mlx_ptr, 1400, 1400, "MLX Test")
+print("MLX ptr:", mlx_ptr)
+print("WIN ptr:", win_ptr)
 
-# Draw a visible 50x50 white square in the middle
-for y in range(175, 225):
-    for x in range(175, 225):
-        m.mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0xFFFFFF)
+#creates image in memory
+img_ptr = m.mlx_new_image(mlx_ptr, 400, 400)
 
+#gives raw pixel buffer modify the buffer
+buf, bpp, size_line, fmt = m.mlx_get_data_addr(img_ptr)
+
+# Sync before writing
+m.mlx_sync(mlx_ptr, m.SYNC_IMAGE_WRITABLE, img_ptr)
+
+
+row = bytes([0, 0, 255, 255] * 400) # BGRX: B=0 G=0 R=255 
+for y in range(400): 
+    buf[y * size_line : y * size_line + 400 * 4] = row
+
+# copies image to the window
+def render(param):
+    m.mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 0, 0)
+    return 0
+
+
+print("bpp:", bpp)
+print("size_line:", size_line)
+print("format:", fmt)
+
+m.mlx_loop_hook(mlx_ptr, render, None)
 m.mlx_hook(win_ptr, 17, 0, close_hook, None)
 m.mlx_key_hook(win_ptr, key_hook, None)
 
 # Start the loop
 m.mlx_loop(mlx_ptr)
+
+
 

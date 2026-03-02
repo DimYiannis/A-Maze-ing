205c61aaf266e1c36170ee8ea5176b5da21332e0
Gianni Lieuw-A-Soe <glieuw-a@f1r1s17.codam.nl>
2026-03-02 16:30:55 +0100

    2

--- maze/test_maze_algo.py
+++ maze/test_maze_algo.py
@@ -1,17 +1,75 @@
-from maze_algo import has_wall, NORTH, EAST, SOUTH, WEST
+from maze_algo import has_wall, add_wall, remove_wall, NORTH, EAST, SOUTH, WEST
 
 
-def test_wall_north():
+def test_wall_north() -> None:
     assert has_wall(9, NORTH)
 
 
-def test_wall_east():
+def test_wall_east() -> None:
     assert has_wall(3, EAST)
 
 
-def test_wall_south():
+def test_wall_south() -> None:
     assert has_wall(6, SOUTH)
 
 
-def test_wall_west():
+def test_wall_west() -> None:
     assert has_wall(9, WEST)
+
+
+def test_add_wall_north():
+    maze = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    add_wall(maze, 1, 0, NORTH)
+    assert maze[1][0] & NORTH
+    assert maze[0][0] & SOUTH
+
+
+def test_add_wall_east():
+    maze = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    add_wall(maze, 0, 1, EAST)
+    assert maze[0][1] & EAST
+    assert maze[0][2] & WEST
+
+
+def test_add_wall_south():
+    maze = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    add_wall(maze, 1, 0, SOUTH)
+    assert maze[1][0] & SOUTH
+    assert maze[2][0] & NORTH
+
+
+def test_add_wall_west():
+    maze = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    add_wall(maze, 0, 1, WEST)
+    assert maze[0][1] & WEST
+    assert maze[0][0] & EAST
+
+
+def test_no_neighbour_north():
+    maze = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    add_wall(maze, 0, 0, NORTH)
+    assert maze[0][0] & NORTH
+
+
+def test_no_neighbour_east():
+    maze = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    add_wall(maze, 0, len(maze[0]) - 1, EAST)
+    assert maze[0][len(maze[0]) - 1] & EAST
+
+
+def test_remove_wall_north():
+    maze = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    add_wall(maze, 1, 0, NORTH)
+    assert maze[1][0] & NORTH
+    remove_wall(maze, 1, 0, NORTH)
+    assert not (maze[1][0] & NORTH)
+    assert not (maze[0][0] & SOUTH)
+
+
+def test_remove_wall_east():
+    maze = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    add_wall(maze, 0, 1, EAST)
+    assert maze[0][1] & EAST
+    remove_wall(maze, 1, 0, EAST)
+    assert not (maze[0][1] & EAST)
+    assert not (maze[0][0] & WEST)

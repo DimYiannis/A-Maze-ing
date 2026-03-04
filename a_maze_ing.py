ad63e429b73db295e6801ecd17e5e41d81e48b09
yiannis <107484245+DimYiannis@users.noreply.github.com>
2026-03-04 18:40:14 +0100

    managed to connect maze gen with display but need to fix the gen of output file and continue fro there

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,7 +1,11 @@
-
-from display.window import main
-
+import sys
+from maze.maze_algo import main as _generate
+from display.window import main as display
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: python3 a_maze_ing.py config.txt")
+        sys.exit(1)
+    _generate()        # reads sys.argv[1] itself
+    display("maze.txt")  # reads the output file

82c308fbf729930696ecf6cb0e1469032c3f7a40
yiannis <107484245+DimYiannis@users.noreply.github.com>
2026-03-04 19:01:43 +0100

    succesfully called regen, deleted main in window.py mazedisplay object is passed instead

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,11 +1,13 @@
 import sys
-from maze.maze_algo import main as _generate
-from display.window import main as display
+from maze.maze_algo import main as generate
+from display.window import MazeDisplay
 
+def regenerate():
+    generate()
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python3 a_maze_ing.py config.txt")
         sys.exit(1)
-    _generate()        # reads sys.argv[1] itself
-    display("maze.txt")  # reads the output file
+    generate()        # reads sys.argv[1] itself
+    MazeDisplay("maze.txt", on_regen=regenerate).run()

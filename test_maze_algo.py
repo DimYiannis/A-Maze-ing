8fce7fe687bc12a31bf0028fbad9c2f3355826c0
yiannis <107484245+DimYiannis@users.noreply.github.com>
2026-03-05 14:41:54 +0100

    clear up

--- test_maze_algo.py
+++
@@ -1,7 +0,0 @@
-from maze_algo import NORTH, EAST, SOUTH, WEST, has_wall 
-
-def test_north_wall_present():
-def test_east_wall_present():
-def test_south_wall_present():
-def test_west_wall_present():
-
